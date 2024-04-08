fcdaae60229519b94b807d3b436348fcff2636c8
Soham Jog <jogsoham2003@gmail.com>
2024-04-08 10:40:32 -0400

    create react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+     <h1 className="bg-black text-white text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
